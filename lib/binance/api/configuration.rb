fc2c746304793b75bb13bf65cf00e485b995a873
Eaven <eaven@popstand.com>
2018-02-19 09:40:04 -0800

    make read api key read/write

--- lib/binance/api/configuration.rb
+++ lib/binance/api/configuration.rb
@@ -5,15 +5,16 @@ module Binance
   module Api
     class Configuration
       class << self
-        attr_writer :api_key, :read_info_api_key, :trading_api_key, :withdrawals_api_key
+        attr_accessor :read_info_api_key
+        attr_writer :api_key, :trading_api_key, :withdrawals_api_key
         attr_writer :secret_key
-        
+
         def api_key(type: nil)
           raise Error.new(message: "invalid security_type type: #{type}.") unless type.nil? || api_key_types.include?(type)
           instance_api_key(type: type) || ENV["BINANCE_#{type.to_s.humanize.upcase}_API_KEY"] ||
           instance_api_key             || ENV["BINANCE_API_KEY"]
         end
-        
+
         def secret_key
           instance_variable_get("@secret_key") || ENV['BINANCE_SECRET_KEY']
         end
